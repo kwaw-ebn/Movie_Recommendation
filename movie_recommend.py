5ddf2c6d4d7055d12facb872672ac17bc735ebae
Ebenezer Kwaw <ekwaw4545@gmail.com>
2025-07-10 17:54:38 +0000

    Add files via upload

--- movie_recommend.py
+++ movie_recommend.py
@@ -8,13 +8,58 @@ import pandas as pd
 import os
 
 # ---------------- Load Pickled Data ----------------
-@st.cache_resource
+import joblib
+import streamlit as st
+
+import pickle
+import joblib
+
+@st.cache_data
 def load_data():
-    movies = pickle.load(open("artifacts/movie_list.pkl", "rb"))
-    similarity = pickle.load(open("artifacts/similarity.pkl", "rb"))
+    movies = joblib.load("artifacts/movie_list_compressed.joblib")
+    similarity = joblib.load("artifacts/similarity_compressed.joblib")
+    return movies, similarity
+    # movies = pickle.load(open("artifacts/movie_list.pkl", "rb"))
+    # similarity = pickle.load(open("artifacts/similarity.pkl", "rb"))
     return movies, similarity
 
 movies, similarity = load_data()
+
+
+# Make sure artifacts folder exists
+import streamlit as st
+import os
+import gdown
+import joblib
+
+# Make sure artifacts folder exists
+if not os.path.exists("artifacts"):
+    os.makedirs("artifacts")
+
+# Google Drive file IDs
+movie_list_id = "1aTaFxMtggJiCHJBpzC1bZrXT5Gccmmbo"
+similarity_id = "1p_Y0LjDV8stiUaNsDZvzECJ6rydgt4yR"
+
+# File paths
+movie_list_path = "artifacts/movie_list_compressed.joblib"
+similarity_path = "artifacts/similarity_compressed.joblib"
+
+# Download files if not already present
+if not os.path.exists(movie_list_path):
+    gdown.download(f"https://drive.google.com/uc?id={movie_list_id}", movie_list_path, quiet=False)
+
+if not os.path.exists(similarity_path):
+    gdown.download(f"https://drive.google.com/uc?id={similarity_id}", similarity_path, quiet=False)
+
+# Load the data
+@st.cache_data
+def load_data():
+    movies = joblib.load(movie_list_path)
+    similarity = joblib.load(similarity_path)
+    return movies, similarity
+
+movies, similarity = load_data()
+
 
 # --- Sidebar ---
 with st.sidebar:
